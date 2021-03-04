fa074207649e0f175d837f859c70f7152d06b4e6
merlinrom <bgr.romina@gmail.com>
2021-03-04 13:05:27 +0330

    dare tamom mishe

--- idkwhy.py
+++ idkwhy.py
@@ -1,9 +1,33 @@
-def A():
-    print("A")
+import tkinter as tk
+from tkinter import Message, ttk
 
-def B():
-    def C():
-        print('C')
-    print('B')
-    A()
-    C()
+def check():
+    print()
+
+def r(a=None):
+    if note.tab(note.select(), "text") == "Register":
+        print("r")
+
+def l(a=None):
+    if note.tab(note.select(), "text") == "Log-in":
+        print("l")
+
+root = tk.Tk()
+note = ttk.Notebook()
+note.grid(row=0, column=0)
+register = tk.Frame(note)
+login = tk.Frame(note)
+note.add(register, text='Register')
+note.add(login, text='Log-in')
+
+b1 = tk.Button(register, text='r', command=r)
+b1.grid(row=0, column=0)
+b1.bind("<Return>", r)
+
+b2 = tk.Button(login, text='l', command=l)
+b2.grid(row=0, column=0)
+b2.bind("<Return>", l)
+
+tk.Button(root, text='check', command=check).grid(row=1, column=0)
+
+root.mainloop()
